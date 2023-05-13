dcff3c5f50492d70847569fa7470fa3ff8c0a75a
victshev <victshev259@gmail.com>
2023-05-13 17:12:20 +0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int cost = 5500;
+        int milePrice = 20;
+        int earnedMiles = cost / milePrice;
+
+        System.out.println("Начисленные мили: " + earnedMiles);
+        
+    }
+}
+
